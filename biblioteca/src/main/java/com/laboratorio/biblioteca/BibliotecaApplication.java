49192834bb35c4f4feda09d05d8d80184f313e2c
Andres Felipe Buitrago Feria <felipebuitrago75@gmail.com>
2019-10-05 11:49:06 -0500

    Se establecen modificaciones en el POM y la clase principal

--- biblioteca/src/main/java/com/laboratorio/biblioteca/BibliotecaApplication.java
+++ biblioteca/src/main/java/com/laboratorio/biblioteca/BibliotecaApplication.java
@@ -2,8 +2,13 @@ package com.laboratorio.biblioteca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@ComponentScan(basePackages = "com.laboratorio.biblioteca")
 public class BibliotecaApplication {
 
 	public static void main(String[] args) {
